926b757cc145b71afc398eb7e741b6004a2e50ba
B <b@feewew.local>
2019-06-19 23:43:48 -0400

    First working solution

--- image-blur-2.rb
+++ image-blur-2.rb
@@ -1,18 +1,37 @@
-# Previous Image class
+# Image class with blur
 # By Brian Kay
 
 class Image
 
   def initialize(arr)
-    @pixels_nested_array = arr
+    @pixels = arr
   end
 
   def output_image
-    @pixels_nested_array.each do |pixel_array|
-      puts "#{pixel_array.join}"
+    @pixels.each do |row|
+      puts "#{row.join}"
     end
   end
 
+  def blur_image
+    coords = []
+    @pixels.each_with_index do |x, i|
+      x.each_with_index do |y, j|
+        if y == 1
+          coords << [i,j]
+        end
+      end
+    end
+
+    coords.each do |x|
+      row = x[0]
+      col = x[1]
+      @pixels[row-1][col] = 1 if @pixels[row-1][col]
+      @pixels[row][col+1] = 1 if @pixels[row][col+1]
+      @pixels[row+1][col] = 1 if @pixels[row+1][col]
+      @pixels[row][col-1] = 1 if @pixels[row][col-1]
+    end
+  end
 end
 
 image = Image.new([
@@ -21,5 +40,15 @@ image = Image.new([
   [0, 0, 0, 1],
   [0, 0, 0, 0]
 ])
+
+puts image.inspect
+
+image.output_image
+
+
+
+image.blur_image
+
+puts image.inspect
 
 image.output_image
