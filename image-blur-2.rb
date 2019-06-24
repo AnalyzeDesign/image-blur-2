98c811cbcc5a5cadd52cea167f78fd2b12865722
B <b@feewew.local>
2019-06-24 15:32:53 -0400

    Image Blur 2: Image class with Blur method added

--- image-blur-2.rb
+++ image-blur-2.rb
@@ -13,42 +13,61 @@ class Image
     end
   end
 
+
   def blur_image
-    coords = []
-    @pixels.each_with_index do |x, i|
-      x.each_with_index do |y, j|
+    new_pixels = []
+
+    @pixels.each_with_index do |x, row|
+      new_pixels[row] = x.clone
+
+      x.each_with_index do |y, col|
+
         if y == 1
-          coords << [i,j]
+          new_pixels[row][col] = 1
+          if col - 1 >= 0
+            new_pixels[row][col-1] = 1
+          end
+          if row - 1 >= 0
+            new_pixels[row-1][col] = 1
+          end
+
+        elsif y == 0
+          if row - 1 >= 0 && @pixels[row-1][col] == 1
+            new_pixels[row][col] = 1
+          end
+          if col - 1 >= 0 && @pixels[row][col-1] == 1
+            new_pixels[row][col] = 1
+          end
+
         end
+
       end
     end
 
-    coords.each do |x|
-      row = x[0]
-      col = x[1]
-      @pixels[row-1][col] = 1 if @pixels[row-1][col]
-      @pixels[row][col+1] = 1 if @pixels[row][col+1]
-      @pixels[row+1][col] = 1 if @pixels[row+1][col]
-      @pixels[row][col-1] = 1 if @pixels[row][col-1]
-    end
+    @pixels = new_pixels
+
   end
+
 end
 
-image = Image.new([
-  [0, 0, 0, 0],
-  [0, 1, 0, 0],
-  [0, 0, 0, 1],
-  [0, 0, 0, 0]
+
+image2 = Image.new([
+  [0,0,0,0],
+  [0,0,1,0],
+  [0,1,0,0],
+  [0,0,0,0],
+  [0,0,0,0],
+  [1,0,0,0]
 ])
 
-puts image.inspect
+puts image2.inspect
 
-image.output_image
+image2.output_image
 
+puts "Blurred:"
 
+image2.blur_image
 
-image.blur_image
+puts image2.inspect
 
-puts image.inspect
-
-image.output_image
+image2.output_image
